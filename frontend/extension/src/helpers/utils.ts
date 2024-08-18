7c9757df266b8f302ab10656f2f7eb900221cd9b
iONLY100 <42662830+iONLY100@users.noreply.github.com>
2024-08-18 18:18:55 +0800

    Update utils.ts

--- frontend/extension/src/helpers/utils.ts
+++ frontend/extension/src/helpers/utils.ts
@@ -1,7 +1,7 @@
 export const getFaviconWithGoogleS2 = (url: string) => {
   try {
     const urlObject = new URL(url);
-    return `https://formidable-scarlet-whale.faviconkit.com/${urlObject.hostname}/128`;
+    return `https://formidable-scarlet-whale.faviconkit.com/${urlObject.hostname}/256`;
   } catch (error) {
     return undefined;
   }
